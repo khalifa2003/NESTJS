6c98f885dadf77ec25285b42f292f6a2bb49b2f7
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2024-08-29 18:30:34 +0300

    Finish Frontend

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Order } from './orders.schema';
 import { Cart } from '../user/cart/cart.schema';
-import { User } from '../user/user.schema';
 import { Product } from '../product/product.schema';
 
 @Injectable()
