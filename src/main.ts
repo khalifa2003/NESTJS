9c0011cb6b2936cde6cce44f7501ac0d5ad9999e
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2025-01-05 21:19:52 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://programming-area.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
